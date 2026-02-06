55af6cdb76d5d0b1cf95def2ba92d45e32df8584
Andrej <pytpoint@gmail.com>
2026-02-06 15:19:22 +0100

    task_2_not_done

--- task_2.py
+++ task_2.py
@@ -1,8 +1,38 @@
 import re
 
-
 def generator_numbers(text: str):
-    zp = re.findall(r"\d+\.\d+", text)
+    pattern = r"\d+\.\d+"
 
-def generator_numbers(text: str):
-    zp = re.findall(r"\d+\.\d+", text)
+    for i in re.finditer(pattern, text):
+        yield float(i.group())
+
+def sum_profit(text, func):
+    numbers_generator = func(text)
+    
+    return sum(numbers_generator)
+
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+
+total_income = sum_profit(text, generator_numbers)
+print(total_income)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
